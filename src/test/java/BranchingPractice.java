0e13fbe5cfe862be036dddf505e96344de1fc3e6
Nuriddin Rakhmanov <nuriddin.rakhmanov@gmail.com>
2021-05-25 17:23:38 -0600

    things we have done so far

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,6 +1,13 @@
 public class BranchingPractice {
     public static void main(String[] args) {
-        
+
+        // created new project
+        //enable VCS
+        // added .gitignore with .idea, target and iml file
+        //made initial commit
+        //shared project on github
+
+
     }
 
 }
